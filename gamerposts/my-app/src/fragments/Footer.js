3768bc1325fa5cc2d812b0f261d494dc78ae9948
Terrelle Thomas <110430142+TerrelleAThomas@users.noreply.github.com>
2023-07-11 03:07:11 -0400

    Yes

---
+++ gamerposts/my-app/src/fragments/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="py-3 my-4 border-top">
+            <p className="text-center">© 2023 GamerPosts, Inc</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ gamerposts/my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/js/bootstrap.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'mdb-ui-kit/css/mdb.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
